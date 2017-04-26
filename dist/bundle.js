7b428dd395d07225227b33afb6e0d0222413b98c
eugenioclrc <eugenioclrc@gmail.com>
2017-04-26 20:06:12 -0300

    clean

---
+++ dist/bundle.js
@@ -0,0 +1,44 @@
+webpackJsonp([0],{
+
+/***/ 119:
+/* unknown exports provided */
+/* all exports used */
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ (function(module, exports) {
+
+"use strict";
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/home/eugenio/www/games/phaser-es6-webpack-master/src/main.js'");
+
+/***/ }),
+
+/***/ 120:
+/* unknown exports provided */
+/* all exports used */
+/*!***************************************!*\
+  !*** ./~/babel-polyfill/lib/index.js ***!
+  \***************************************/
+/***/ (function(module, exports) {
+
+"use strict";
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/home/eugenio/www/games/phaser-es6-webpack-master/node_modules/babel-polyfill/lib/index.js'");
+
+/***/ }),
+
+/***/ 312:
+/* unknown exports provided */
+/* all exports used */
+/*!******************************************!*\
+  !*** multi babel-polyfill ./src/main.js ***!
+  \******************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! babel-polyfill */120);
+module.exports = __webpack_require__(/*! /home/eugenio/www/games/phaser-es6-webpack-master/src/main.js */119);
+
+
+/***/ })
+
+},[312]);
+//# sourceMappingURL=bundle.js.map
